ee8a75f4ef1e9fbdb29e9211e97cb42ed7433b1c
Eladie <eladie.ibumbar@gmail.com>
2022-08-08 21:16:50 +0300

    Projects-tab

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navBar/Navbar'
 import Footer from '../components/footer/Footer'
+import HeroImg2 from '../components/hero2/HeroImg2'
 
-const Project = () => {
+const Project = (props) => {
   return (
     <div>
       <Navbar/>
+      <HeroImg2 heading='Projects.' text="Some of my most recent projects"/>
       <Footer/>
     </div>
   )
