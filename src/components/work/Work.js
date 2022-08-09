3e3ae6baf73585c3d4ac95f1abd3fe09550aac21
Eladie <eladie.ibumbar@gmail.com>
2022-08-09 19:08:15 +0300

    proj-final

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,22 +1,49 @@
 import './WorkCard.css'
-import React, { isValidElement } from 'react'
+import React from 'react'
 import WorkCard from './WorkCard'
-import WorkData from './WorkData'
+import pro1 from '../../Img/project1.png'
+import pro2 from '../../Img/project2.png'
+import pro3 from '../../Img/project3.png'
+
+const ProjectData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Web",
+        text: "Some Text, description of project",
+        view: 'portfolio-eladie.vercel.app',
+        source: 'https://github.com/Eladie/Portfolio-Eladie'
+    },
+    {
+        imgsrc: pro2,
+        title: "Ecommerce Web",
+        text: "Some Text, description of project",
+        view: 'portfolio-eladie.vercel.app',
+        source: 'https://github.com/Eladie/Portfolio-Eladie'
+    },
+    {
+        imgsrc: pro3,
+        title: "Ecommerce Web",
+        text: "Some Text, description of project",
+        view: 'portfolio-eladie.vercel.app',
+        source: 'https://github.com/Eladie/Portfolio-Eladie'
+    },
+   
+];
 
 const Work = () => {
   return (
     <div className='work-container'>
         <h1 className='project-heading'>Projects</h1>
         <div className='project-container'>
-            {WorkData.map((val,ind) => {
+            {ProjectData.map((elem,ind) => {
                 return (
                     <WorkCard 
                     key={ind}
-                    imgsrc={val.imgsrc}
-                    title={val.title}
-                    text={val.text}
-                    view={val.view}
-                    source={val.source}/>
+                    imgsrc={elem.imgsrc}
+                    title={elem.title}
+                    text={elem.text}
+                    view={elem.view}
+                    source={elem.source}/>
                 )
             })}
         </div>
